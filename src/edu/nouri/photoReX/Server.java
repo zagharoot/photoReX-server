e59b0881102ccacc0b187bf0ca98cce76788cf94
Ali Nouri <nouri@cs.rutgers.edu>
2012-04-26 21:41:40 -0400

    the previous commit was wrong and incomplete!

--- src/edu/nouri/photoReX/Server.java
+++ src/edu/nouri/photoReX/Server.java
@@ -2,6 +2,7 @@ package edu.nouri.photoReX;
 
 import java.util.*; 
 import redis.clients.jedis.*; 
+import redis.clients.jedis.exceptions.*; 
 import edu.nouri.photoReX.recommender.*; 
 
 
@@ -30,43 +31,57 @@ public class Server {
 		while(true)
 		{
 	
-			RecommendationTask task = new RecommendationTask( redis.blpop(0, "users:recommend:queue")); 
-			System.out.print("recommendingg " + task.pageCount + " pics for '" + task.username + "' ... "); 
-			long start = System.currentTimeMillis();
-			ArrayList<RecommendationPage> recs = learner.recommend(task); 
-			
-			for(int i=0; i< recs.size(); i++)
-			{
-				long page = redis.incr("counter:user:" + task.username + ":page" ); 
-				RecommendationPage rec = recs.get(i); 
-				rec.pageid = page; 
+			try{
+				RecommendationTask task = new RecommendationTask( redis.blpop(0, "users:recommend:queue")); 
+				System.out.print("recommendingg " + task.pageCount + " pics for '" + task.username + "' ... "); 
+				long start = System.currentTimeMillis();
+				ArrayList<RecommendationPage> recs = learner.recommend(task); 
 				
-				
-				//add info to various structures in redis: 
-				Pipeline p = redis.pipelined(); 
-				p.rpush("user:"+task.username + ":queue", rec.toJson()); 	//add json rep to the end of user queu
-
-				//add structured data to redis: 
-				String userVisitedKey = "user:" + task.username + ":visited";
-				String pageKey        = "user:" + task.username + ":page:" + rec.pageid + ":pics"; 
-				for(int j=0; j< rec.pics.size(); j++)
+				for(int i=0; i< recs.size(); i++)
 				{
-					String hash = rec.pics.get(j).picture.hash; 
-					p.sadd(userVisitedKey, hash);						//user visited this photo
+					long page = redis.incr("counter:user:" + task.username + ":page" ); 
+					RecommendationPage rec = recs.get(i); 
+					rec.pageid = page; 
 					
-					String picVisitedKey  = "pic:" +  hash + ":visited"; 
-					p.sadd(picVisitedKey, task.username); 				//picture was visited by this user
 					
-					p.rpush(pageKey, hash); 								//page contains this picture
-					p.set("pic:" + hash, rec.pics.get(j).picture.toJson()); //remember to call toHash before calling toJson (bad design really)
+					//add info to various structures in redis: 
+					Pipeline p = redis.pipelined(); 
+					p.rpush("user:"+task.username + ":queue", rec.toJson()); 	//add json rep to the end of user queu
+	
+					//add structured data to redis: 
+					String userVisitedKey = "user:" + task.username + ":visited";
+					String pageKey        = "user:" + task.username + ":page:" + rec.pageid + ":pics"; 
+					for(int j=0; j< rec.pics.size(); j++)
+					{
+						String hash = rec.pics.get(j).picture.toHash(); 
+						p.sadd(userVisitedKey, hash);						//user visited this photo
+						
+						String picVisitedKey  = "pic:" +  hash + ":visited"; 
+						p.sadd(picVisitedKey, task.username); 				//picture was visited by this user
+						
+						p.rpush(pageKey, hash); 								//page contains this picture
+						p.set("pic:" + hash, rec.pics.get(j).picture.toJson()); //remember to call toHash before calling toJson (bad design really)
+					}
+					
+					p.sync(); 				//wait for commands to execute
 				}
-				
-				p.sync(); 				//wait for commands to execute
+				long end = System.currentTimeMillis(); 
+				System.out.println("done in " + (end-start)/1000.0 + "seconds."); 
+				//send pic to redis both as complete json to be passed to client and detailed structured for analysis 
 			}
-			
-			long end = System.currentTimeMillis(); 
-			System.out.println("done in " + (end-start)/1000.0 + "seconds."); 
-			//send pic to redis both as complete json to be passed to client and detailed structured for analysis 
+			catch(JedisConnectionException jce)
+			{
+				try
+				{
+					redis.disconnect(); 
+					System.out.println("redis was closed. trying to reconnect..."); 
+				}
+				catch(Exception e)
+				{
+					//wait some time 
+//					Thread.sleep(10000); 
+				}
+			}
 		}
 	}
 	
